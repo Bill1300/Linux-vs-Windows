3295b4c5d620c9b0cb4d3f723314b7949806e1c5
Bill1300 <42590905+Bill1300@users.noreply.github.com>
2019-03-21 23:43:35 -0300

    Inicialização...
    Inicialização...

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿function atualizar(n_grafico) {
+  if(n_grafico == 1){
+    var iframe = document.getElementById('grafico1');
+  }
+  if(n_grafico == 2){
+    var iframe = document.getElementById('grafico2');
+  }
+  iframe.src = iframe.src;
+}
