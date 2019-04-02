0364438f5b72e11f86bd991fb26e709c460421b3
Bill1300 <42590905+Bill1300@users.noreply.github.com>
2019-04-02 02:03:59 -0300

    Erro no JS
    Mas ta responsive

--- files/script.js
+++ files/script.js
@@ -1,10 +1,7 @@
 function atualizar(valor) {
-  //tabela-dados
   if(valor == 0){
-    var iframe = document.getElementById('tabela-dados');
+    var iframe = document.getElementById('GSheets-iframe');
+    iframe.src = iframe.src;
   }
-  if(valor == 1){
-    var iframe = document.getElementById('grafico1');
-  }
-  iframe.src = iframe.src;
+
 }
